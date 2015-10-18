77afcda8ae86fefaa3a49ff260aef78941c448ca
lucasart <lucas.braesch@gmail.com>
2015-10-18 09:21:43 +0800

    make.sh [out_file]

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 W="-Wfatal-errors -Wall -Wextra -Wshadow"
 O="-O3 -flto -march=native -DNDEBUG"
-g++ -std=c++11 $W $O -o ./demolito ./src/*.cc -lpthread
+g++ -std=c++11 $W $O -o $1 ./src/*.cc -lpthread
 strip ./demolito
