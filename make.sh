70dba1ebd29b81a13b09e46dcc9ab1ab0a55bf3f
lucasart <lucas.braesch@gmail.com>
2017-06-09 10:32:24 +0800

    Revert musl
    Somehow musl crashes randomly, when compiling with gcc 6.3, but not 5.4. The
    culprit could be musl, but could also be gcc, or even my code (that somehow
    relies on a more robust behavior of glibc that isn't guaranteed by the
    standard). I don't have time to investigate libc crashes, so revert musl for
    now.

--- make.sh
+++ make.sh
@@ -3,7 +3,7 @@ astyle -A3 -s4 -f -xn -xc -xl -xC100 -O ./src/* && rm ./src/*.orig
 VERSION="-DVERSION=\"$(git show -s --format=%ci | cut -d\  -f1)\""
 W="-Wfatal-errors -Wall -Wextra -Wshadow"
 O="-O3 -flto -march=native -DNDEBUG"
-musl-gcc -static $VERSION -std=gnu11 $W $O -o $1 ./src/*.c -lpthread -lm
+gcc -static $VERSION -std=gnu11 $W $O -o $1 ./src/*.c -lpthread -lm
 strip $1
 $1 search 12 1
 
