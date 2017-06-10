6b78cca40644400d08a9c0cae7f915e4c2661fcf
lucasart <lucas.braesch@gmail.com>
2017-06-10 14:35:18 +0800

    Remove -static

--- make.sh
+++ make.sh
@@ -3,8 +3,7 @@ astyle -A3 -s4 -f -xn -xc -xl -xC100 -O ./src/* && rm ./src/*.orig
 VERSION="-DVERSION=\"$(git show -s --format=%ci | cut -d\  -f1)\""
 W="-Wfatal-errors -Wall -Wextra -Wshadow"
 O="-O3 -flto -march=native -DNDEBUG"
-gcc -static $VERSION -std=gnu11 $W $O -o $1 ./src/*.c -lpthread -lm
-strip $1
+gcc $VERSION -std=gnu11 $W $O -o $1 ./src/*.c -lpthread -lm -s
 $1 search 12 1
 
 # For Android
