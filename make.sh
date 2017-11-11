ed4bab0cad46dd0313d393e1b4374c0969a4b449
lucasart <lucas.braesch@gmail.com>
2017-11-11 20:45:32 +0800

    Use Clang by default +4.5% speedup on the standard Linux compile.

--- make.sh
+++ make.sh
@@ -2,6 +2,6 @@ rm -f $1
 VERSION="-DVERSION=\"$(git show -s --format=%ci | cut -d\  -f1)\""
 W="-Wfatal-errors -Wall -Wextra -Wshadow"
 O="-O3 -flto -march=native -DPEXT -DNDEBUG"
-C="gcc"  # Linux: gcc, Windows: x86_64-w64-mingw32-gcc -static, Android: arm-linux-gnueabi-gcc -static
+C="clang"  # Linux: clang/gcc, Windows: x86_64-w64-mingw32-gcc -static, Android: arm-linux-gnueabi-gcc -static
 $C $VERSION -std=gnu11 $W $O -o $1 ./src/*.c -lpthread -lm -s
 $1 search 12 1
