d38f49d3a49181ca884dc25022bf3f9c7b9a02b9
lucasart <lucas.braesch@gmail.com>
2017-11-19 18:08:07 +0800

    LMP at depth=2 (21080385)
    SPRT(0,4), tc=4+0.04, hash=4:
    
    test vs master: 701 - 558 - 1243  [0.529] 2502
    ELO difference: 19.88 +/- 9.65
    SPRT: llr 2.96, lbound -2.94, ubound 2.94 - H1 was accepted

--- src/search.c
+++ src/search.c
@@ -326,7 +326,7 @@ static int search(Worker *worker, const Position *pos, int ply, int depth, int a
 
         // Prune losing captures and lated moves, near the leaves
         if (depth <= 4 && !pvNode && !pos->checkers && !nextPos.checkers
-                && (see < 0 || (depth == 1 && moveCount >= 5))
+                && (see < 0 || (depth <= 2 && moveCount >= 5 * depth))
                 && !move_is_capture(pos, currentMove))
             continue;
 
