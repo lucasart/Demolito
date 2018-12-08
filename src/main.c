e9f936c6af5588064b1af4e023586480fe8c319d
lucasart <lucas.braesch@gmail.com>
2018-12-08 14:54:30 +0800

    Hash parameter for search and pert (13010564)
    No functional change:
    
    $ hash search 12 1 1 > /dev/null
    kn/s: 2925
    total = 13625873
    
    But now hash=2 is the default value (faster on depth=12 run):
    $ ./hash search 12 > /dev/null
    kn/s: 2895
    total = 13010564
    
    So the bench changes.

--- src/main.c
+++ src/main.c
@@ -27,14 +27,14 @@
 #include "smp.h"
 #include "uci.h"
 
-uint64_t test(bool perft, int depth, int threads)
+uint64_t test(bool perft, int depth, int threads, int hash)
 {
     static const char *fens[] = {
         #include "test.csv"
         NULL
     };
 
-    hash_resize(1);
+    hash_resize(hash);
     uint64_t result = 0, nodes;
     smp_resize(threads);
     uciChess960 = true;
@@ -78,9 +78,11 @@ int main(int argc, char **argv)
     smp_resize(1);
 
     if (argc >= 2) {
-        if ((!strcmp(argv[1], "perft") || !strcmp(argv[1], "search")) && argc >= 4) {
-            const int depth = atoi(argv[2]), threads = atoi(argv[3]);
-            const uint64_t nodes = test(!strcmp(argv[1], "perft"), depth, threads);
+        if ((!strcmp(argv[1], "perft") || !strcmp(argv[1], "search")) && argc >= 3) {
+            const int depth = atoi(argv[2]);
+            const int threads = argc > 3 ? atoi(argv[3]) : 1;
+            const int hash = argc > 4 ? atoi(argv[4]) : 2;
+            const uint64_t nodes = test(!strcmp(argv[1], "perft"), depth, threads, hash);
             fprintf(stderr, "total = %" PRIu64 "\n", nodes);
         }
     } else
