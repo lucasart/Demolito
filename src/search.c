03acc6fac776f3777aa7e39811cddce8175836e9
lucasart <lucas.braesch@gmail.com>
2020-06-28 15:54:56 +0800

    Prune quiet moves with negative history at depth 1
    tc=8+0.08, hash=8:
    LLR: 2.96 (-2.94, 2.94) [0.00, 5.00]
    Games: 19888 W: 4574 L: 4342 D: 10972
    
    tc=32+0.32, hash=32:
    LLR: 2.97 (-2.94, 2.94) [0.00, 5.00]
    Games: 7144 W: 1443 L: 1284 D: 4417
    
    seal  : e34508aefbd1c22e
    nodes : 11653094

--- src/search.c
+++ src/search.c
@@ -345,6 +345,10 @@ static int search(Worker *worker, const Position *pos, int ply, int depth, int a
             // Late Move Pruning
             if (!capture && depth <= 4 && moveCount >= 3 * depth + 2 * improving)
                 break;
+
+            // Prune quiet moves with negative history at depth=1 (excluding 1st move)
+            if (!capture && depth == 1 && moveCount >= 2 && sort.scores[sort.idx - 1] < 0)
+                break;
         }
 
         hash_prefetch(nextPos.key);
